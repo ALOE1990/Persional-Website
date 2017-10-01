c628f55208e1e8fca00f61d3914984f6e72ec40b
pan <gpaloe1990@gmail.com>
2017-09-30 22:45:46 -0700

    change port

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var app = express();
 var path = require('path');
 
 app.use(express.static('public'));
+app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res){
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
 
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
     console.log('web server is listening on port 3000');
 });
